f905cfe3ca2f31cf79028e4f0e7fb7eb345fa13d
Tony Nguyen <tonywins35@gmail.com>
2025-04-23 15:18:24 -0400

    add_airpor

--- add_airplane.py
+++ add_airplane.py
@@ -5,7 +5,7 @@ import mysql.connector
 conn = mysql.connector.connect(
     host="127.0.0.1",
     user="root",
-    password="",
+    password="!",
     database="flight_tracking"
 )
 cursor = conn.cursor()
